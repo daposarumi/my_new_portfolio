93b5f744b3e0910222cecc043f5447bf29ca1141
Dapo Sarumi <sarumiadedapo@gmail.com>
2025-07-14 18:49:07 +0300

    remove

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Manrope } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
 import { getPages } from "@/sanity/sanity-utils";
 import Header from "./components/header";
 
