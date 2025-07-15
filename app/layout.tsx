3836accad56a36fa835366d7a6a63e996d265750
Dapo Sarumi <sarumiadedapo@gmail.com>
2025-07-15 14:29:09 +0300

    colour

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
   const pages = await getPages();
 
   return (
-    <html lang="en" className="light">
+    <html lang="en" className="bg-white">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${manrope.className} antialiased flex flex-col min-h-screen`}
       >
